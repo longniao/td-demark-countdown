29d9f3b8bfab6d8020fe44f3e63d77deb8c82f8c
Lukas Stephan <lukas.stephan@SA12.local>
2018-04-27 23:47:01 +0100

    TDST

--- demarkindicators/demarkcountdown/DemarkCountdown.py
+++ demarkindicators/demarkcountdown/DemarkCountdown.py
@@ -1,4 +1,8 @@
 from strategy import Strategy
+from pprint import pprint
+import numpy as np
+MAX_SEQUENTIAL_LOOKAHEAD = 5
+
 class DemarkCountdown():
     def __init__(self,price_history):
         self.price_history = price_history
@@ -10,6 +14,8 @@ class DemarkCountdown():
         cache["BULLISH_PRICE_FLIPS"] = {}
         cache["TD_SELL_SETUPS"] = {}
         cache["TD_BUY_SETUPS"] = {}
+        cache["TDST_SUPPORT"] = (None,None)
+        cache["TDST_RESISTANCE"] = (None,None)
         return cache
     
     def bearish_td_price_flip(self):
@@ -23,7 +29,7 @@ class DemarkCountdown():
         indices = []
 
         pattern_size = 6
-        for i in range(len(price_history) - pattern_size + 2):
+        for i in range(len(price_history) - pattern_size + 1):
             if price_history[i]["close"] < price_history[i + 4]["close"] and \
                 price_history[i+1]["close"] > price_history[i + 5]["close"]:
                 indices.append(i)
@@ -40,13 +46,10 @@ class DemarkCountdown():
         price_history = self.price_history["candles"]
         indices = []
 
-
         pattern_size = 6
-        for i in range(len(price_history) - pattern_size + 2):
-            print i
+        for i in range(len(price_history) - pattern_size + 1):
             if price_history[i]["close"] > price_history[i + 4]["close"] and \
                 price_history[i+1]["close"] < price_history[i + 5]["close"]:
-                print "here"
                 indices.append(i)
         self.cache["BULLISH_PRICE_FLIPS"] = {"indices": indices}
         return
@@ -61,19 +64,57 @@ class DemarkCountdown():
 
         price_history = self.price_history["candles"]
         indices = []
+        perfect_indicies = []
 
-        pattern_size = 13
-        for i in range(len(price_history) - pattern_size):
-            # Starting from i, lookback 9 bars
-            for j in range(9):
-                if not price_history[i+j]["close"] > price_history[i+j+4]["close"]:
-                    break
-                if j == 8:
-                    indices.append(i)
-        self.cache["TD_SELL_SETUPS"] = {"indices": indices}
+        # Must be preceded by bullish price flip
+        bullish_td_price_flip_indices = self.cache["BULLISH_PRICE_FLIPS"]["indices"]
+        for price_flip_index in bullish_td_price_flip_indices:
+            if price_flip_index > 8:
+                for j in range(9):
+                    if not price_history[price_flip_index-j]["close"] > price_history[price_flip_index-j+4]["close"]:
+                        break
+                    if j == 8:
+                        bar_nine_index = price_flip_index - j
+                        indices.append(bar_nine_index)
+                        perfect_indicies.append(self.perfect_td_sell_setup(bar_nine_index))
+        
+        most_recent_sell_setup = price_history[indices[0]:indices[0]+9]
+        lows = [candle["low"] for candle in most_recent_sell_setup]
+        low = min(lows)
+        low_index = lows.index(low) + indices[0]
+        self.cache["TDST_SUPPORT"] = (low, low_index)
+
+        self.cache["TD_SELL_SETUPS"] = {"indices": indices, "perfect" : perfect_indicies}
+        print pprint(self.cache)
         return
 
-            
+    def perfect_td_sell_setup(self,bar_nine_index):
+        """
+        Returns True if sell setup has been perfected
+
+        :return bool: True if sell setup is perfect
+        """
+        price_history = self.price_history["candles"]
+        if ( \
+                (price_history[bar_nine_index]["close"] > price_history[bar_nine_index + 3]["close"] and price_history[bar_nine_index]["close"] > price_history[bar_nine_index + 2]["close"]) or \
+                (price_history[bar_nine_index + 1]["close"] > price_history[bar_nine_index + 3]["close"] and price_history[bar_nine_index + 1]["close"] > price_history[bar_nine_index + 2]["close"]) or \
+                self.delayed_sell_setup_perfection(bar_nine_index)
+            ):
+            return True
+        return False
+
+    def delayed_sell_setup_perfection(self,bar_nine_index):
+        
+        # Look five bars ahead MAX_SEQUENTIAL_LOOKAHEAD
+        price_history = self.price_history["candles"]
+        for index in range(1,MAX_SEQUENTIAL_LOOKAHEAD):
+            if bar_nine_index - index < 0: # If index out of range
+                return False
+            if price_history[bar_nine_index - index]["close"] > price_history[bar_nine_index + 3]["close"] and \
+                price_history[bar_nine_index - index]["close"] > price_history[bar_nine_index + 2]["close"]:
+                return True
+        return False
+
 
     def td_buy_setup(self):
         """
@@ -82,19 +123,58 @@ class DemarkCountdown():
         :return list: Indicies of last_x TD Buy Setups
         """
 
-        price_history = self.price_history
+        price_history = self.price_history["candles"]
         indices = []
+        perfect_indicies = []
 
-        pattern_size = 13
-        for i in range(len(price_history) - pattern_size):
-            # Starting from i, lookback 9 bars
-            for j in range(9):
-                if not price_history[i+j]["close"] < price_history[i+j+4]["close"]:
-                    break
-                if j == 8:
-                    indices.append(i)
-        self.cache["TD_BUY_SETUPS"] = {"indices": indices}
+        # Must be preceded by bearish price flip
+        bearish_td_price_flip_indices = self.cache["BEARISH_PRICE_FLIPS"]["indices"]
+        for price_flip_index in bearish_td_price_flip_indices:
+            if price_flip_index > 8:
+                for j in range(9):
+                    if not price_history[price_flip_index-j]["close"] < price_history[price_flip_index-j+4]["close"]:
+                        break
+                    if j == 8:
+                        bar_nine_index = price_flip_index - j
+                        indices.append(bar_nine_index)
+                        perfect_indicies.append(self.perfect_td_buy_setup(bar_nine_index))
+        
+        most_recent_buy_setup = price_history[indices[0]:indices[0]+9]
+        highs = [candle["high"] for candle in most_recent_buy_setup]
+        high = max(highs)
+        high_index = highs.index(high) + indices[0]
+        self.cache["TDST_RESISTANCE"] = (high, high_index)
+
+        self.cache["TD_BUY_SETUPS"] = {"indices": indices, "perfect" : perfect_indicies}
+        print pprint(self.cache)
         return
+
+    def perfect_td_buy_setup(self,bar_nine_index):
+        """
+        Returns True if sell setup has been perfected
+
+        :return bool: True if sell setup is perfect
+        """
+        price_history = self.price_history["candles"]
+        if ( \
+                (price_history[bar_nine_index]["close"] < price_history[bar_nine_index + 3]["close"] and price_history[bar_nine_index]["close"] < price_history[bar_nine_index + 2]["close"]) or \
+                (price_history[bar_nine_index + 1]["close"] < price_history[bar_nine_index + 3]["close"] and price_history[bar_nine_index + 1]["close"] < price_history[bar_nine_index + 2]["close"]) or \
+                self.delayed_buy_setup_perfection(bar_nine_index)
+            ):
+            return True
+        return False
+
+    def delayed_buy_setup_perfection(self,bar_nine_index):
+        
+        # Look five bars ahead MAX_SEQUENTIAL_LOOKAHEAD
+        price_history = self.price_history["candles"]
+        for index in range(1,MAX_SEQUENTIAL_LOOKAHEAD):
+            if bar_nine_index - index < 0: # If index out of range
+                return False
+            if price_history[bar_nine_index - index]["close"] < price_history[bar_nine_index + 3]["close"] and \
+                price_history[bar_nine_index - index]["close"] < price_history[bar_nine_index + 2]["close"]:
+                return True
+        return False
 
 
 
--- demarkindicators/demarkcountdown/tests/test_demark_countdown.py
+++ demarkindicators/demarkcountdown/tests/test_demark_countdown.py
@@ -33,15 +33,7 @@ class PriceFlipUnitTest(unittest.TestCase):
         pass
 
 class TDSetupUnitTest(unittest.TestCase):
-    def test_bullish_setup_single_pattern(self):
-        pass
-    def test_bullish_setup_double_pattern(self):
-        pass
-    def test_bullish_broken_setup(self):
-        pass
-    def test_bullish_perfect_setup(self):
-        pass
-    def test_sell_setup_single_pattern(self):
+    def test_sell_setup_single_perfect_pattern(self):
         candles = {}
         with open(curr_dir + "bullish_price_flip_and_sell_setup.json","r") as f:
             candles = json.loads(f.read())
@@ -49,15 +41,34 @@ class TDSetupUnitTest(unittest.TestCase):
         
         demark = DemarkCountdown(candles)
         self.assertEquals(candles["candles"][0]["close"],56.312)
-
+        demark.bullish_td_price_flip()
         demark.td_sell_setup()
         cache = demark.cache
-        self.assertEquals(cache["TD_SELL_SETUPS"]["indices"][0],0)
-    def test_bearish_setup_double_pattern(self):
-        pass
+        self.assertEquals(cache["TD_SELL_SETUPS"]["indices"][0],1)
+        self.assertEquals(cache["TD_SELL_SETUPS"]["perfect"][0],True)
+        self.assertEquals(cache["TDST_SUPPORT"],(47.024, 9))
+        self.assertEquals(demark.price_history["candles"][1]["close"],55.937)
+    def test_sell_and_buy_setups(self):
+        candles = {}
+        with open(curr_dir + "sell_setup_and_buy_setup.json","r") as f:
+            candles = json.loads(f.read())
+        candles = candlesConverter.oanda(candles)
+        
+        demark = DemarkCountdown(candles)
+        self.assertEquals(candles["candles"][0]["close"],63.774)
+        demark.bullish_td_price_flip()
+        demark.td_sell_setup()
+        demark.bearish_td_price_flip()
+        demark.td_buy_setup()
+        cache = demark.cache
+        pprint(cache)
+
+        self.assertEquals(demark.price_history["candles"][cache["TD_SELL_SETUPS"]["indices"][0]]["close"],55.937)
+        self.assertEquals(demark.price_history["candles"][cache["TD_BUY_SETUPS"]["indices"][0]]["close"],64.083)
+        #self.assertEquals(True,False)
     def test_bearish_broken_setup(self):
         pass
-    def test_bearish_perfect_setup(self):
+    def test_perfect_sell_setup(self):
         pass
 
 if __name__ == "__main__":
